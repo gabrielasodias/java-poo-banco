2aed039861af8e17bff6a42f105b93c36ec0c3d1
Gabriela Dias <gabrieladiasofc@gmail.com>
2024-08-14 15:50:43 -0300

    classe Conta e classe para testes

--- src/br/com/poobanco/Conta.java
+++ src/br/com/poobanco/Conta.java
@@ -1,10 +1,29 @@
 package br.com.poobanco;
 
 public class Conta {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	int numero;
+	int agencia;
+	double saldo;
+	
+	public Conta() {
+		
 	}
-
+	
+	public Conta(int numero, int agencia, double saldo) {
+		this.numero = numero;
+		this.agencia = agencia;
+		this.saldo = saldo;
+	}
+	
+	public void depositar(double valor) {
+		this.saldo += valor;
+	}
+	
+	public void retirar(double valor) {
+		this.saldo -= valor;
+	}
+	
+	public double verificarSaldo() {
+		return saldo;
+	}
 }
---
+++ src/br/com/poobanco/Teste.java
@@ -0,0 +1,22 @@
+package br.com.poobanco;
+
+public class Teste {
+
+	public static void main(String[] args) {
+		
+		Conta cc = new Conta();
+		cc.saldo = 50;
+		cc.agencia = 123;
+		cc.numero = 323;
+		
+		cc.depositar(100);
+		
+		System.out.println(cc.verificarSaldo());
+		
+		Conta poupanca = new Conta(111, 222, 1000);
+		
+		poupanca.retirar(50);
+		
+		System.out.println(poupanca.verificarSaldo());
+	}
+}
